4383d0c146d16d652d893e36b9f6c2530b99ff73
bini59 <myuni0234@gmail.com>
2022-11-06 23:46:09 +0900

    fix : cors setting
    set cors *

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var channelRouter = require('./routes/api/channel');
+
 var app = express();
 
 // view engine setup
@@ -19,8 +21,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/youtube/channel', channelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
